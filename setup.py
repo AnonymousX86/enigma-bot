eef64e0891fc8b252a966caf1aa1d0844d80129c
AnonymousX86 <jakub.suchenek.25@gmail.com>
2020-10-15 23:38:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
 with open('requirements.txt') as f:
@@ -5,12 +6,12 @@ with open('requirements.txt') as f:
 
 setup(
     name='3N1GM4',
-    version='0.1',
+    version='0.2',
     author='Jakub S.',
     # url='',
     description='Multipurpose Discord bot',
     license='GPL-3.0',
     packages=['enigma', 'enigma.cogs', 'enigma.utils'],
-    python_requires='>=3.8.1',
+    python_requires='>=3.7.6',
     install_requires=requirements
 )
