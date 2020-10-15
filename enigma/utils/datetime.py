b7fd963cefb3ceb306b6874101da9be8e592f810
AnonymousX86 <jakub.suchenek.25@gmail.com>
2020-10-15 23:39:22 +0200

    Fix docstrings

--- enigma/utils/datetime.py
+++ enigma/utils/datetime.py
@@ -2,8 +2,7 @@ from datetime import datetime as d, timezone
 
 
 def utc_to_local(utc_dt: d) -> d:
-    """
-    Converts 'datetime' object timezone to UTC (+00:00).
+    """Converts 'datetime' object timezone to UTC (+00:00).
 
     :param utc_dt: Local datetime object.
     :return: UTC (+00:00) datetime object.
@@ -13,8 +12,7 @@ def utc_to_local(utc_dt: d) -> d:
 
 
 def execute_time(start_time) -> str:
-    """
-    Count time between 'start_time' and current timestamp.
+    """Counts time between 'start_time' and current timestamp.
 
     :param start_time: Start time datetime object.
     :return: Formatted execute time in ms.
