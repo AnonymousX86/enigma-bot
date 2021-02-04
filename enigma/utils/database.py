d38b568842ea39d1a52cba22e68d9966fe33a3d7
AnonymousX86 <jakub.suchenek.25@gmail.com>
2021-02-04 21:41:13 +0100

    Optimize imports

--- enigma/utils/database.py
+++ enigma/utils/database.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from datetime import date, datetime as d
-from typing import List, Optional, Union, Tuple
+from typing import List, Optional
 
-from sqlalchemy import create_engine, Column, Integer, Date, BigInteger, Text, text
+from sqlalchemy import create_engine, Column, Integer, Date, BigInteger, Text
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
