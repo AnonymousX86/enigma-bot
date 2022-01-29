88a8bb3251d638c3a649a38fe309105127a3df2b
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2022-01-29 15:12:49 +0100

    Fix wrong url and catch no url exception

--- enigma/settings/database.py
+++ enigma/settings/database.py
@@ -3,4 +3,9 @@ from os import environ as env
 
 
 def database_url() -> str:
-    return env.get('DATABASE_URL')
+    url = env.get('DATABASE_URL')
+    if not url:
+        raise RuntimeError("No database url found")
+    if url.startswith("postgres://"):
+        url = url.replace("postgres://", "postgresql://")
+    return url
